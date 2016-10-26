0fe845f7532b8b033d8632ddbca79dea2a50bf2f
xin.cai <xin.cai@dianrong.com>
2016-10-26 18:01:28 +0800

    workaround for cash callback

--- hero/src/main/java/com/hero/HeroWebView.java
+++ hero/src/main/java/com/hero/HeroWebView.java
@@ -68,6 +68,15 @@ public class HeroWebView extends WebView implements IHero {
                     return true;
                 }
 
+                if (url.startsWith("https://cashloan-callback.dianrong.com")) {
+                    try {
+                        HeroWebView.this.on("{command:'back'}");
+                        return true;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
                 if(!url.startsWith("http:") || url.startsWith("https:") ) {
                     return false;
                 }
