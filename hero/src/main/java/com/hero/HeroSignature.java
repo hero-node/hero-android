e749395d1f40670fa2e9e7eb1daaf9e6f33a5571
yuri.zhang <insectalan@126.com>
2019-01-04 11:49:55 +0800

    move HeroInstance to HeroSignature

--- hero/src/main/java/com/hero/HeroSignature.java
+++ hero/src/main/java/com/hero/HeroSignature.java
@@ -79,6 +79,45 @@ public class HeroSignature extends View implements IHero, FingerprintHelper.Simp
     private PopupWindow popupWindow;
     private boolean isCancel = false;
 
+    private static class HeroInstance {
+
+        private static volatile HeroInstance heroInstance;
+
+        private String public_key;
+
+        private String private_key;
+
+        private HeroInstance() {}
+
+        public static HeroInstance getInstance() {
+            if (heroInstance == null) {
+                synchronized (HeroInstance.class) {
+                    if (heroInstance == null) {
+                        heroInstance = new HeroInstance();
+                    }
+                }
+            }
+            return heroInstance;
+        }
+
+        public String getPublic_key() {
+            return public_key;
+        }
+
+        public void setPublic_key(String public_key) {
+            this.public_key = public_key;
+        }
+
+        public String getPrivate_key() {
+            return private_key;
+        }
+
+        public void setPrivate_key(String private_key) {
+            this.private_key = private_key;
+        }
+
+    }
+
     public HeroSignature(Context c) {
         super(c);
         this.context = c;
